33e3c347be000a2a33e3e182b342ceb1f3e67aca
Ralf Ebert <info@ralfebert.de>
2020-06-23 17:20:23 +0200

    Initial Commit

---
+++ ExampleApp/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Ralf Ebert on 23.06.20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Ralf Ebert on 23.06.20.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
